3b6641fc6c074d59f66f3b6cfdd4011998808b6d
mikamvra <mika.mvra@gmail.com>
2026-05-22 13:40:18 -0400

    side class completed

--- src/main/java/org/example/Side.java
+++ src/main/java/org/example/Side.java
@@ -1,4 +1,17 @@
 package org.example;
 
 public class Side {
+    private String type;
+
+    public Side(String type) {
+        this.type = type;
+    }
+
+    public double getPrice() {
+        return 1.50;
+    }
+
+    public String getDescription() {
+        return "Side: " + type + " - $" + String.format("%.2f", getPrice());
+    }
 }
