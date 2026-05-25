79194f708a38052a43a5faf1fa6509d32d9fa604
mikamvra <mika.mvra@gmail.com>
2026-05-25 19:05:04 -0400

    completed ordermanager class and started some  methods

--- src/main/java/org/example/OrderManager.java
+++ src/main/java/org/example/OrderManager.java
@@ -1,5 +1,13 @@
 package org.example;
 
+import javax.imageio.IIOException;
+import javax.swing.text.DateFormatter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -25,5 +33,44 @@ public class OrderManager {
             else if (item instanceof Side) total += ((Side) item).getPrice();
         }
         return total;
+    }
+    public void saveReceipt (double total){
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter fileFomatter = DateTimeFormatter.ofPattern("yyyyMMdd-HHmm");
+        DateTimeFormatter displayFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+
+        String fileName ="Receipt/" + now.format(fileFomatter) + ".txt";
+        File directory = new File("receipts");
+        if (!directory.exists()) {
+            directory.mkdir();
+        }
+        try (FileWriter writer = new FileWriter(fileName)){
+            String receiptText = "==================================\n" +
+                    "        Mika Pho Life             " +
+                    "    Date/Time: " + now.format(displayFormatter) + "\n" +
+                    "==================================\n\n";
+            for (Object  item : currentOrder){
+                if(item instanceof PhoBowl){
+                    PhoBowl bowl = (PhoBowl) item;
+                    receiptText = receiptText + bowl.getDescirption() + "\n";
+                } else if (item instanceof Drink) {
+                    Drink drink = (Drink) item;
+                    receiptText = receiptText + drink.getDescription() + "\n";
+                } else if (item instanceof Side) {
+                    Side side = (Side) item;
+                    receiptText = receiptText + side.getDescription() + "\n";
+
+                }
+            }
+            receiptText = receiptText + "----------------------------------------\n" +
+                    "TOTAL PAID: $" + String.format("%.2f", total) + "\n" +
+                    "=================================\n";
+
+            writer.write(receiptText);
+            System.out.println("Receipt saved successfully to: " + fileName);
+
+        }catch (IOException e){
+            System.out.println("Error saving receipt" + e.getMessage());
+        }
     }
 }
